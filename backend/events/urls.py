d826afcabdf5ce3e56cff371c098aa2fe60457dc
Juanc <3393@holbertonstudents.com>
2024-05-29 22:00:38 -0500

    feat: :zap: Implemented maintenance data view
    Added new html file to display sse functionality to display maintenance data

--- backend/events/urls.py
+++ backend/events/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import sse_stream, index
+from .views import sse_stream, index, maintenance_sse_stream, alternative
 
 
 urlpatterns = [
-    path("",index, name="index"),
-    path("stream/", sse_stream, name="sse_stream")
+    path("", index, name="index"),
+    path("alternative/", alternative, name="alternative"),
+    path("stream/", sse_stream, name="sse_stream"),
+    path("mstream/", maintenance_sse_stream, name="maintenance_sse_stream"),
 ]
--- backend/events/views.py
+++ backend/events/views.py
@@ -3,22 +3,58 @@ import random
 
 from django.http import StreamingHttpResponse
 from django.shortcuts import render
+from .models import Maintenance
 
 
 async def sse_stream(request):
     """
     Sends server-sent events to the client.
     """
+
     async def event_stream():
         emojis = ["🚀", "🐎", "🌅", "🦾", "🍇"]
         i = 0
         while True:
-            yield f'data: {random.choice(emojis)} {i}\n\n'
+            yield f"data: {random.choice(emojis)} {i}\n\n"
             i += 1
             await asyncio.sleep(1)
 
-    return StreamingHttpResponse(event_stream(), content_type='text/event-stream')
+    return StreamingHttpResponse(event_stream(), content_type="text/event-stream")
+
+
+async def maintenance_sse_stream(request):
+    """
+    Sends server-sent events to the client.
+    """
+
+    async def event_stream():
+        while True:
+            last_maintenance = await asyncio.to_thread(
+                lambda: Maintenance.objects.filter(status="active").last()
+            )
+
+            if last_maintenance:
+                start = last_maintenance.start
+                while True:
+                    yield f"data: {start}\n\n"
+                    # Check if the status is still active, otherwise break the loop
+                    if not await asyncio.to_thread(
+                        lambda: Maintenance.objects.filter(
+                            id=last_maintenance.id, status="active"
+                        ).exists()
+                    ):
+                        break
+                    await asyncio.sleep(1)
+            else:
+                # Sleep for a while before checking again if there is no active maintenance
+                await asyncio.sleep(1)
+
+    return StreamingHttpResponse(event_stream(), content_type="text/event-stream")
 
 
 def index(request):
-    return render(request, 'sse/events.html')
+    return render(request, "sse/events.html")
+
+
+def alternative(request):
+    return render(request, "sse/maintenance_sse.html")
