9c71af88cd045d7efcd807fc5b6101aaf0e23f58
Juanc <3393@holbertonstudents.com>
2024-06-03 15:49:49 -0500

    Added into cors allowed origins the front application running on port 3000

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -25,8 +25,11 @@ SECRET_KEY = "django-insecure-dq4^_8qg^_0i3l9a2ea7gq-o)d-ddn%w5honm-&=erk)h!rkju
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 
 # Application definition
 
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "rest_framework",
     "events",
 ]
@@ -45,6 +49,7 @@ INSTALLED_APPS = [
 ASGI_APPLICATION = "core.asgi.application"
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
